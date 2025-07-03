86e74fe700c764637bfe66ee0d1f6de1b4c190e0
whatreallifeis <1sinkina1@mail.ru>
2025-07-03 18:59:15 +0300

    Добавлен клиентский интерфейс

--- SevenLab/SevenLab/Form2.Designer.cs
+++ SevenLab/SevenLab/Form2.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(284, 128);
+            label1.Name = "label1";
+            label1.Size = new Size(259, 20);
+            label1.TabIndex = 0;
+            label1.Text = "изображение бурной деятельности";
+            label1.Click += label1_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "Form2";
+            Text = "Form2";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
